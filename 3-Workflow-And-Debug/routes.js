f8a256ae4d75ef32989d797042d5450eda898fe2
kuhachi <n-hieu@plott.co.jp>
2024-05-01 12:21:56 +0700

    tst

---
+++ 3-Workflow-And-Debug/routes.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>tester</title></head>");
+        res.write("<body><form action='/message' method='POST'><input name='message' type='text'/> <button type='submit'>ok</button></form></body>");
+        res.write("</html>");
+        return res.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        const body = [];
+
+        req.on("data", (chunk) => {
+            console.log({chunk});
+            body.push(chunk);
+        });
+
+        req.on("end", () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split("=")[1];
+            fs.writeFileSync("message.txt", message, (err) => {
+                console.log({err});
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                return res.end();
+            });
+        });
+    }
+
+    // process.exit();
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>tester</title></head>");
+    res.write("<body><h1>Hello from NodeJs</h1></body>");
+    res.write("</html>");
+    res.end();
+}
+
+exports.handler = requestHandler;
+exports.someText = "Some hard coded text";
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = "Some hard coded text";
+//
+// module.exports = {
+//     handler: requestHandler,
+//     someText: "Some hard coded text"
+// };
