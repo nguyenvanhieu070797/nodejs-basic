5880403a64046c5fe35f874f133255b566a55621
n-hieu <nguyenvanhieu070797@gmail.com>
2024-05-03 01:55:06 +0700

    test

--- 3-Workflow-And-Debug/app.js
+++ 3-Workflow-And-Debug/app.js
@@ -1,32 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use('/add-product', (req, res, next) => {
-    console.log("add-product");
-    res.send(`<form action="/product" method="POST">
-        <input type="text" name="title"/>
-        <button type="submit">
-            Ok!
-        </button>
-    </form>`);
+app.use('/admin', adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-
-app.post('/product', (req, res, next) => {
-    console.log({body: req.body});
-    res.redirect("/");
-});
-
-app.use('/', (req, res, next) => {
-    console.log("middleware");
-    res.send("<h1>Hellow from Express</h1>");
-});
-
-
 
 app.listen(3000);
 
---
+++ 3-Workflow-And-Debug/routes/admin.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const rootDir = require("../util/path");
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, "views", "add-product.html"))
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+    res.redirect("/");
+});
+
+module.exports = router;
